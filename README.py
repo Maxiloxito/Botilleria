493d7a5249af99e1b24a6571f1eb4b4fb742aec0
Maxiloxito <86543315+Maxiloxito@users.noreply.github.com>
2022-10-31 11:56:31 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,21 +1,47 @@
 # Botilleria
 
-class
 
-class Products
+class Product
 {
 	#Atributos
 
-	product_id= #Producto
-
-	product_quantity= #Cantidad de producto
-	product_price= #Precio del producto
-	product_sold= #Producto vendido
-	amount_product_sold= #Cantidad de producto vendido
-	product_expiration_date= #Fecha de expiracion del producto
+	id= #Codigo de barra del producto (marca, nombre del producto)
+	supplier='' #Distribuidor 
+	price= #Precio del producto
+	content= #Contenido del producto (En gramos o en mililitros)
+	category[''] #Categoria con la que se guardara el producto(Alcohol, abarrotes, dulces, etc...)
+	
 
 	#Metodos
 
-	change_product_quantity(
+	change_supplier() #Cambiar distruibor o mantenerlo
+	change_category() #aaa
+}
 
+class Inventory
+{
+	#Atributos
+	
+	product_id= #Codigo de barra del producto (Marca, nombre del producto)
+	product_category= #Categoria de los productos
+	quantity_category= #Cantidad de producto en 'N' categoria
+	stock= #Cantidad de producto
+	purchase_price= #Precio de compra de producto
+	sale_price= #Precio de venta
+	amount_sold= #Cantidad de producto vendido
+	total_sales= #Cantidad de ventas de producto
+	
+	#Métodos
+	
+	show_info() #Mostrar los datos del producto
+	change_info() #Actualizar datos del producto
+	update_quantity([enteroption]) #Actualizacion de cantidades (categoria,stock)
+	update_sale_price() #Actualizar el precio de venta del producto
+	update_purchase_price() #Actualizar el precio de compra
+	add_producto() #nuevo producto ingresado
+	add_category() #Nueva categoria creada
+	profit() #Ganancia de las ventas
+	
+	
+	
 }
